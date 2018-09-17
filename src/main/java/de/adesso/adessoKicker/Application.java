1c72ba21fc57acaa436377411e3c157072f2586c
Plotogandr <35301804+Plotogandr@users.noreply.github.com>
2018-09-17 13:39:42 +0200

    Renamed AdessoKickerApplication

--- src/main/java/de/adesso/adessoKicker/Application.java
+++ src/main/java/de/adesso/adessoKicker/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdessoKickerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdessoKickerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
