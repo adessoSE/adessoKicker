ba90ad037011b0d58cf2292030110beac26400b7
Plotogandr <35301804+Plotogandr@users.noreply.github.com>
2018-09-17 13:07:46 +0200

    Repo setup

---
+++ src/main/java/de/adesso/adessoKicker/AdessoKickerApplication.java
@@ -0,0 +1,12 @@
+package de.adesso.adessoKicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdessoKickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdessoKickerApplication.class, args);
+	}
+}
