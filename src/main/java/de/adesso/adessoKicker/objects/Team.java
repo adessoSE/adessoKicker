426c6864237669f866187b06857cbfd4b25f5728
caylak <caylak@DE-ADN-B1Y91G2.adesso.local>
2018-09-25 12:45:59 +0200

    Added ErrorMsgs for teamadd.html

--- src/main/java/de/adesso/adessoKicker/objects/Team.java
+++ src/main/java/de/adesso/adessoKicker/objects/Team.java
@@ -1,7 +1,15 @@
 package de.adesso.adessoKicker.objects;
 
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "team")
@@ -11,6 +19,8 @@ public class Team {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long teamId;
 
+    @NotNull
+    @Size(min=1, max=30)
     private String teamName;
 
     private long teamWins;
@@ -33,6 +43,8 @@ public class Team {
         this.teamName = teamName;
         this.playerA = playerA;
         this.playerB = playerB;
+        this.teamWins = 0;
+        this.teamLosses = 0;
     }
 
     public long getTeamId() {
