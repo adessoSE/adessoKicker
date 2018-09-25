da94d2b0b7e5739f86d4e5546f87824cbea6e828
Plotogandr <florian.czarnecki@adesso.de>
2018-09-25 09:49:36 +0200

    Added missing import

--- src/main/java/de/adesso/adessoKicker/objects/Tournament.java
+++ src/main/java/de/adesso/adessoKicker/objects/Tournament.java
@@ -5,17 +5,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
