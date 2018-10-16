1b671f2109a0741964fc3af52b13ad8310364f8d
Jan Schneider <jan.schneider@adesso.de>
2018-10-16 12:57:38 +0200

    Added test suite (allows to test multiple tests)

---
+++ src/test/java/de/adesso/masterTest/MasterTest.java
@@ -0,0 +1,16 @@
+package de.adesso.masterTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//Run this class as JUnit test for testing a test suite
+@RunWith (Suite.class)
+
+//Enter test here for testing
+@Suite.SuiteClasses({
+	
+})
+
+public class MasterTest {
+
+}
