94e7f177b512356ecac2ef8059b30aef796481a2
Jan Schneider <jan.schneider@adesso.de>
2018-10-19 16:07:30 +0200

    Notification Repository update

--- src/main/java/de/adesso/kicker/notification/NotificationRepository.java
+++ src/main/java/de/adesso/kicker/notification/NotificationRepository.java
@@ -1,6 +1,23 @@
 package de.adesso.kicker.notification;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import de.adesso.kicker.notification.Notification.NotificationType;
+import de.adesso.kicker.user.User;
+
 public interface NotificationRepository extends CrudRepository<Notification, Long> {
+	
+	//Get a Notification by a specific id
+	Notification findByNotificationId(Long id);
+	
+	//Get all Notifications that were send from a specific user
+	List<Notification> findBySender(User sender);
+	
+	//Get all Notifications that were received by a specific user
+	List<Notification> findByReceiver(User sender);
+	
+	//Gets all Notifications by a specific type
+	List<Notification> findByNotificationType(NotificationType notificationType);
 }
