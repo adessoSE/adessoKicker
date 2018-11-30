c00c0b158c69c48cfa691e1a885ca396d7e84171
Jan Schneider <jan.schneider@adesso.de>
2018-11-30 11:38:47 +0100

    Added more Notification Tests

--- src/test/java/de/adesso/kicker/notification/NotificationServiceTest.java
+++ src/test/java/de/adesso/kicker/notification/NotificationServiceTest.java
@@ -1,16 +1,15 @@
 package de.adesso.kicker.notification;
 
-import de.adesso.kicker.notification.teamjoinrequest.TeamJoinRequestService;
 import de.adesso.kicker.user.User;
 import de.adesso.kicker.user.UserDummy;
-import de.adesso.kicker.user.UserRepository;
 import de.adesso.kicker.user.UserService;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
@@ -30,15 +29,21 @@ public class NotificationServiceTest {
     User user = new UserDummy().alternateUser();
     User otherUser = new UserDummy().alternateUser2();
 
-    @Before
+    Notification notification = new NotificationDummy().defaultNotification();
+
+    @BeforeEach
     public void setUp(){
 
         MockitoAnnotations.initMocks(this);
 
         //Say what to return when trying to get a user via UserService
-        when(userService.getUserById(anyLong())).thenReturn(user);
+        when(userService.getUserById(anyLong())).thenReturn(null);
         when(userService.getUserById(eq(user.getUserId()))).thenReturn(user);
         when(userService.getUserById(eq(otherUser.getUserId()))).thenReturn(otherUser);
+
+        //Say what to return when trying to get a notification via NotificationService
+        when(notificationRepository.findByNotificationId(anyLong())).thenReturn(null);
+        when(notificationRepository.findByNotificationId(eq(notification.getNotificationId()))).thenReturn(notification);
     }
 
     //Test --> Try adding a notification (type = Notification)
@@ -66,5 +71,22 @@ public class NotificationServiceTest {
     public void saveNotification_NullSenderAndReceiver(){
 
         notificationService.saveNotification(anyString(), -1L, -1L);
+    }
+    @Test
+    public void saveNotification_Object(){
+
+        notificationService.saveNotification(notification);
+    }
+    @Test
+    public void getNotificationById_ValidInput(){
+
+        Notification n = notificationService.getNotificationById(notification.getNotificationId());
+        assertEquals(notification, n);
+    }
+    @Test
+    public void getNotificationById_Null(){
+
+        Notification n = notificationService.getNotificationById(-1L);
+        assertEquals(null, n);
     }
 }
