c123da7a5d34ad8ef02ff9f38c159d6337722d97
Cem Caylak <cem.caylak@adesso.de>
2019-02-19 14:53:33 +0100

    Add Given/When/Then scructure to UserServiceTest

--- src/main/java/de/adesso/kicker/user/exception/UserNotFoundException.java
+++ src/main/java/de/adesso/kicker/user/exception/UserNotFoundException.java
@@ -1,5 +1,8 @@
 package de.adesso.kicker.user.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException() {
         super("User does not exist");
