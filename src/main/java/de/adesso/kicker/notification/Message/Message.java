a2d950954fb962c08bd5dc5cb21b9066e2d78438
Florian Albers <Florian.Albers@adesso.de>
2019-02-22 06:59:49 +0100

    Altered notification check to orElseThrow

--- src/main/java/de/adesso/kicker/notification/Message/Message.java
+++ src/main/java/de/adesso/kicker/notification/Message/Message.java
@@ -11,7 +11,8 @@ public class Message extends Notification {
 
     private MessageType messageType;
 
-    public Message() { }
+    public Message() {
+    }
 
     public Message(User sender, User receiver, MessageType messageType) {
         super(sender, receiver);
