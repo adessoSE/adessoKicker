2d0eea3a71808dcc2b37f9c706a8c46ed4a02e6c
Florian Albers <Florian.Albers@adesso.de>
2019-03-04 10:38:47 +0100

    Added notification object to controllers

--- src/main/java/de/adesso/kicker/site/controller/HomeController.java
+++ src/main/java/de/adesso/kicker/site/controller/HomeController.java
@@ -1,5 +1,6 @@
 package de.adesso.kicker.site.controller;
 
+import de.adesso.kicker.notification.service.NotificationService;
 import de.adesso.kicker.ranking.service.RankingService;
 import de.adesso.kicker.user.exception.UserNotFoundException;
 import de.adesso.kicker.user.service.UserService;
@@ -17,6 +18,8 @@ public class HomeController {
 
     private final RankingService rankingService;
 
+    private final NotificationService notificationService;
+
     @GetMapping(value = { "/", "/home", "/ranking" })
     public ModelAndView ranking(@RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
@@ -25,11 +28,14 @@ public class HomeController {
         try {
             var user = userService.getLoggedInUser();
             var rank = rankingService.getPositionOfPlayer(user.getRanking());
+            var notifications = notificationService.getNotificationsByReceiver(user);
             modelAndView.addObject("user", user);
             modelAndView.addObject("rank", rank);
+            modelAndView.addObject("notifications", notifications);
         } catch (UserNotFoundException e) {
             modelAndView.addObject("user", false);
             modelAndView.addObject("rank", false);
+            modelAndView.addObject("notifications", false);
         }
         modelAndView.addObject("users", users);
         modelAndView.setViewName("sites/ranking.html");
