a5115ccd8666bd5cdc7ff466bb7a111fed5fd350
Jan Schneider <jan.schneider@adesso.de>
2019-03-04 12:25:06 +0100

    Added new custom radio buttons

--- src/main/resources/static/js/matchresult.js
+++ src/main/resources/static/js/matchresult.js
@@ -2,13 +2,23 @@ $(document).ready(function () {
     // Toggle tooltip
     $('[data-toggle="tooltip"]').tooltip();
 
-    // Shows/Disables trophy on select
-    $("#form-check-1").click(function () {
-        $('#radio2-icon').removeClass('trophy-selected');
-        $('#radio1-icon').addClass('trophy-selected');
+    // Shows/Disables trophies on click
+    var icon1 = $('#radio1-icon');
+    var icon2 = $('#radio2-icon');
+    var radio1 = $("#radio1");
+    var radio2 = $("#radio2");
+    $('#form-check-1').click(function () {
+        toggleTrophies(icon2, icon1, radio2, radio1);
     });
-    $("#form-check-2").click(function () {
-        $('#radio1-icon').removeClass('trophy-selected');
-        $('#radio2-icon').addClass('trophy-selected');
+    $('#form-check-2').click(function () {
+        toggleTrophies(icon1, icon2, radio1, radio2);
     });
+    function toggleTrophies(selectedIcon, otherIcon, selectedRadio, otherRadio){
+        selectedIcon.removeClass('trophy-selected');
+        selectedIcon.addClass('outline-trophy');
+        otherIcon.removeClass('outline-trophy');
+        otherIcon.addClass('trophy-selected');
+        selectedRadio.prop("checked", false);
+        otherRadio.prop("checked", true);
+    }
 });
