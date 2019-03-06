7a12faa6b5eef65e05cff8c8fd1c26f590d03367
Jan Schneider <jan.schneider@adesso.de>
2019-03-06 12:00:44 +0100

    Added notification counter

--- src/main/resources/static/js/notifications.js
+++ src/main/resources/static/js/notifications.js
@@ -1,3 +1,13 @@
+function updateBadgeCount(){
+    var notificationBadge = document.getElementById('notification-badge');
+    var temp = parseInt(notificationBadge.getAttribute('value')) - 1;
+    notificationBadge.setAttribute('value', temp);
+    notificationBadge.innerText = notificationBadge.getAttribute('value');
+    //Hide badge if no notifications
+    if (temp <= 0){
+        notificationBadge.hidden = true;
+    }
+}
 
 function accept(btn) {
     var notification = btn.parentNode.parentNode.parentNode;
@@ -6,6 +16,7 @@ function accept(btn) {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             notification.remove();
+            updateBadgeCount();
         }
     }
     xhttp.open("GET", "/notifications/accept/" + id, false);
@@ -19,7 +30,9 @@ function decline(btn) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            alert();
             notification.remove();
+            updateBadgeCount();
         }
     }
     xhttp.open("GET", "/notifications/decline/" + id, false);
