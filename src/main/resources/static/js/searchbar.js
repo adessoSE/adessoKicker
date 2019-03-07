d5df25ab7ed04625f276dcb864f9ba597aec124f
Jan Schneider <jan.schneider@adesso.de>
2019-03-07 16:01:47 +0100

    'searchbar.js' works now with multiple searchbars

--- src/main/resources/static/js/searchbar.js
+++ src/main/resources/static/js/searchbar.js
@@ -1,19 +1,33 @@
 $(document).ready(function () {
-    //When clicking on a seachbar
-    $(".searchbar input").on("focus", function () {
-        $(".searched-user").toggle();
+
+    var searchbarInput = $(".search-bar input");
+
+    //Display 'search-bar-content' when clicking in a searchbar input
+    searchbarInput.on("focus", function () {
+        $(this).parents('.search-bar').find(".search-bar-content").toggle();
     });
-    //Leaving' the search bar
-    $(".searchbar input").on("blur", function () {
-        //Short delay -> workaround to allow clicking on searched users without toggling them so you cant click them
-        setTimeout(function(){$(".searched-user").toggle();}, 200);
+
+    //Hide 'search-bar-content' when clicking anywhere else (loses focus)
+    searchbarInput.on("blur", function () {
+        var thisSearchbarInput = this;
+        /* When you click on an item in the 'search-bar-content' the 'search-bar-input' loses focus.
+         * Causing the 'search-bar-content' to vanish, so the action that should happen doesn't work.
+         * As the 'blur-event' happens before the 'click-event'. A small delay is a workaround to prevent this.
+         */
+        setTimeout(function(){$(thisSearchbarInput).parents('.search-bar').find(".search-bar-content").toggle();}, 200);
     });
-    //When typing a char in the seachbar
-    $(".searchbar input").on("keyup", function (event) {
+
+    //Filters the list elements of 'search-bar-content' and hides the elements that doesn't match the filter requirements
+    searchbarInput.on("keyup", function (event) {
         var value = $(this).val().toLowerCase();
-        $(".list-group li").filter(function () {
+        $(this).parents('.search-bar').find(".list-group li").filter(function () {
             //indexOf() returns the positions of the char typed in the search bar (returns -1 if word doesnt contain char --> toggle element)
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
+        //Trigger 'onclick' when enter pressed in input field
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            alert('Enter search');
+        }
     });
 });
