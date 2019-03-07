c5abdb6a5e6f09a3ee3956f602cf4e5f75031ba2
Florian Czarnecki <florian.czarnecki@adesso.de>
2019-03-07 14:20:57 +0100

    Fix errors and formatting in JS files

--- src/main/resources/static/js/notifications.js
+++ src/main/resources/static/js/notifications.js
@@ -1,15 +1,15 @@
-function updateBadgeCount(){
+function updateBadgeCount() {
     var notificationBadge = document.getElementById('notification-badge');
     var temp = parseInt(notificationBadge.getAttribute('value')) - 1;
     notificationBadge.setAttribute('value', temp);
     notificationBadge.innerText = notificationBadge.getAttribute('value');
     //Hide badge if no notifications
-    if (temp <= 0){
+    if (temp <= 0) {
         notificationBadge.hidden = true;
     }
 }
 
-function removeNotification(btn){
+function removeNotification(btn) {
     var notification = btn.parentNode.parentNode.parentNode;
     notification.remove();
 }
@@ -17,12 +17,12 @@ function removeNotification(btn){
 function accept(btn) {
     var xhttp = new XMLHttpRequest();
     var id = btn.parentNode.parentNode.parentNode.getAttribute('value');
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
             removeNotification(btn);
             updateBadgeCount();
         }
-    }
+    };
     xhttp.open("GET", "/notifications/accept/" + id, false);
     xhttp.send();
     return false;
@@ -31,12 +31,12 @@ function accept(btn) {
 function decline(btn) {
     var xhttp = new XMLHttpRequest();
     var id = btn.parentNode.parentNode.parentNode.getAttribute('value');
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
             removeNotification(btn);
             updateBadgeCount();
         }
-    }
+    };
     xhttp.open("GET", "/notifications/decline/" + id, false);
     xhttp.send();
     return false;
