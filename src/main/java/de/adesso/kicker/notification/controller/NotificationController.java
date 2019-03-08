2a5e4ede13ffd4ba074b9f5ae76444e2c44c27e8
Florian Albers <Florian.Albers@adesso.de>
2019-03-08 07:01:51 +0100

    Created html for notification result. Created new messages. Updated NotificationController for ModelAndView usage.

--- src/main/java/de/adesso/kicker/notification/controller/NotificationController.java
+++ src/main/java/de/adesso/kicker/notification/controller/NotificationController.java
@@ -1,5 +1,7 @@
 package de.adesso.kicker.notification.controller;
 
+import de.adesso.kicker.notification.exception.NotificationNotExistingException;
+import de.adesso.kicker.notification.exception.WrongReceiverException;
 import de.adesso.kicker.notification.persistence.Notification;
 import de.adesso.kicker.notification.service.NotificationService;
 import de.adesso.kicker.user.service.UserService;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -26,13 +29,33 @@ public class NotificationController {
     }
 
     @GetMapping("/decline/{id}")
-    public void declineNotification(@PathVariable long id) {
-        notificationService.declineNotification(id);
+    public ModelAndView declineNotification(@PathVariable long id) {
+        var modelAndView = new ModelAndView();
+        try {
+            notificationService.declineNotification(id);
+            modelAndView.addObject("successDeclined", true);
+        } catch (NotificationNotExistingException e) {
+            modelAndView.addObject("notExisting", true);
+        } catch (WrongReceiverException e) {
+            modelAndView.addObject("wrongReceiver", true);
+        }
+        modelAndView.setViewName("sites/notificationresult.html");
+        return modelAndView;
     }
 
     @GetMapping("/accept/{id}")
-    public void acceptNotification(@PathVariable long id) {
-        notificationService.acceptNotification(id);
+    public ModelAndView acceptNotification(@PathVariable long id) {
+        var modelAndView = new ModelAndView();
+        try {
+            notificationService.acceptNotification(id);
+            modelAndView.addObject("successAccepted", true);
+        } catch (NotificationNotExistingException e) {
+            modelAndView.addObject("notExisting", true);
+        } catch (WrongReceiverException e) {
+            modelAndView.addObject("wrongReceiver", true);
+        }
+        modelAndView.setViewName("sites/notificationresult.html");
+        return modelAndView;
     }
 
 }
