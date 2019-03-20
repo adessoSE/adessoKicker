ea4071da40e2b0e06b9afbab481b6464b67a5eb5
Jan Schneider <jan.schneider@adesso.de>
2019-03-20 13:20:58 +0100

    Added multiple lines for wins and looses chart

--- src/main/resources/templates/js/profile.js
+++ src/main/resources/templates/js/profile.js
@@ -5,20 +5,32 @@ var x = 0;
 var ranksOverTime = [];
 var diffWinLossesOverTime = [];
 var winRateOverTime = [];
+var winsPerDay = [];
+var lossesPerDay = [];
+
+var _winsLastDay = 0;
+var _lossesLastDay = 0;
 
 // Get data based on user statistics
 [# th:each="statistic : ${statistics}"]
+
     var rank = [[${statistic.rank}]];
     var diffWinLoose = [[${statistic.wins - statistic.losses}]];
     var wins = [[${statistic.wins}]];
     var losses = [[${statistic.losses}]];
+    var winsThisDay = [[${statistic.wins}]] - _winsLastDay;
+    var lossesThisDay = [[${statistic.losses}]] - _lossesLastDay;
     var winRate = parseFloat(wins / losses).toFixed(2);
 
     ranksOverTime.push({x : + x, y : rank});
     diffWinLossesOverTime.push({x : + x, y : diffWinLoose});
+    winsPerDay.push({x : + x, y : winsThisDay});
+    lossesPerDay.push({x : + x, y : lossesThisDay});
     winRateOverTime.push({x : + x, y : winRate});
     dates.push([[${statistic.date}]]);
 
+    _winsLastDay = [[${statistic.wins}]];
+    _lossesLastDay = [[${statistic.losses}]];
     x++;
 [/]
 
@@ -28,7 +40,7 @@ var rankOverTimeChart = new Chart($('#canvas-rank-over-time'), {
         labels: dates,
         datasets: [{
             fill: false,
-            borderColor: "rgb(0,0,255)",
+            borderColor: '#0071B9',
             data: ranksOverTime
         }]
     },
@@ -49,8 +61,8 @@ var winRateOverTimeChart= new Chart($('#canvas-win-rate-over-time'), {
     data: {
         labels: dates,
         datasets: [{
-            fill: false,
-            borderColor: "rgb(0,0,255)",
+            fill: origin,
+            borderColor: '#0071B9',
             data: winRateOverTime
         }]
     },
@@ -63,9 +75,33 @@ var winsAndLossesPerDayChart = new Chart($('#canvas-wins-losses-over-time'), {
     type: 'line',
     data: {
         labels: dates,
+        datasets: [
+            {
+                fill: origin,
+                borderColor: '#0071B9',
+                data: winsPerDay
+            },
+            {
+                fill: origin,
+                borderColor: '#ff2a28',
+                backgroundColor: '#ff7978',
+                data: lossesPerDay
+            }
+            ]
+    },
+    options: {
+        legend: { display: false },
+    }
+});
+
+/* Florian Albers wants to keep this chart
+var winsAndLossesPerDayChart = new Chart($('#canvas-wins-losses-over-time'), {
+    type: 'line',
+    data: {
+        labels: dates,
         datasets: [{
-            fill: false,
-            borderColor: "rgb(0,0,255)",
+            fill: origin,
+            borderColor: '#0071B9',
             data: diffWinLossesOverTime
         }]
     },
@@ -73,4 +109,5 @@ var winsAndLossesPerDayChart = new Chart($('#canvas-wins-losses-over-time'), {
         legend: { display: false }
     }
 });
+*/
 
