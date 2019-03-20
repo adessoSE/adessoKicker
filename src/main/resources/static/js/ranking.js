4b63851badd977f05c03a412eccb9637bd2a5bdf
Florian Albers <Florian.Albers@adesso.de>
2019-03-20 10:00:44 +0100

    Fixed broken links of pager functions Closes#258

--- src/main/resources/static/js/ranking.js
+++ src/main/resources/static/js/ranking.js
@@ -30,7 +30,7 @@ function nextPage() {
     if (size == null) {
         size = 10;
     }
-    location.href = "/statistic?page=" + page + "&size=" + size;
+    location.href = "/ranking?page=" + page + "&size=" + size;
 }
 
 function previousPage() {
@@ -44,7 +44,7 @@ function previousPage() {
     if (size == null) {
         size = 10;
     }
-    location.href = "/statistic?page=" + page + "&size=" + size;
+    location.href = "/ranking?page=" + page + "&size=" + size;
 }
 
 function findGetParameter(parameterName) {
