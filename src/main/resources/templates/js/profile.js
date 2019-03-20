fa5808d4a80aa48b5229b72ad13bf66a7649c370
Jan Schneider <jan.schneider@adesso.de>
2019-03-20 12:26:27 +0100

    Added winRateOverTime chart, and renamed variables

--- src/main/resources/templates/js/profile.js
+++ src/main/resources/templates/js/profile.js
@@ -1,26 +1,31 @@
-
-var ranks = [];
-var wins = [];
+// Dates defines the x axis on each chart
 var dates = [];
 var x = 0;
+// Other data to track in each chart (defines y axis)
+var ranksOverTime = [];
+var diffWinLossesOverTime = [];
+var winRateOverTime = [];
+
+// Get data based on user statistics
 [# th:each="statistic : ${statistics}"]
-var rank = [[${statistic.rank}]];
-var preWins = [[${statistic.wins - statistic.losses}]];
-    ranks.push({x : + x, y : rank});
-    wins.push({x : + x, y : preWins});
+    var rank = [[${statistic.rank}]];
+    var diffWinLoose = [[${statistic.wins - statistic.losses}]];
+    var winRate = [[${statistic.wins / statistic.losses}]];
+    ranksOverTime.push({x : + x, y : rank});
+    diffWinLossesOverTime.push({x : + x, y : diffWinLoose});
+    winRateOverTime.push({x : + x, y : winRate});
     dates.push([[${statistic.date}]]);
     x++;
 [/]
 
-var ctx = $('#canvas-rank');
-var rankOverTimeGraph = new Chart(ctx, {
+var rankOverTimeChart = new Chart($('#canvas-rank-over-time'), {
     type: 'line',
     data: {
         labels: dates,
         datasets: [{
             fill: false,
             borderColor: "rgb(0,0,255)",
-            data: ranks
+            data: ranksOverTime
         }]
     },
     options: {
@@ -35,18 +40,33 @@ var rankOverTimeGraph = new Chart(ctx, {
     }
 });
 
-var cty = $('#canvas-wins');
-var winsOverTimeGraph = new Chart(cty, {
+var winRateOverTimeChart= new Chart($('#canvas-win-rate-over-time'), {
     type: 'line',
     data: {
         labels: dates,
         datasets: [{
             fill: false,
             borderColor: "rgb(0,0,255)",
-            data: wins
+            data: winRateOverTime
         }]
     },
     options: {
         legend: { display: false }
     }
 });
+
+var winsAndLossesPerDayChart = new Chart($('#canvas-wins-losses-over-time'), {
+    type: 'line',
+    data: {
+        labels: dates,
+        datasets: [{
+            fill: false,
+            borderColor: "rgb(0,0,255)",
+            data: diffWinLossesOverTime
+        }]
+    },
+    options: {
+        legend: { display: false }
+    }
+});
+
